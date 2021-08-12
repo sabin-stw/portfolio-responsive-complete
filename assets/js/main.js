47dbe302d298d13138ca9ececa90247ee865d07a
Sabin Simkhada <sbnsim81@gmail.com>
2021-08-12 11:06:37 +0545

    Dark Theme added

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,6 +56,29 @@ sr.reveal('.work__img',{interval: 200});
 /*SCROLL CONTACT*/
 sr.reveal('.contact__input',{interval: 200}); 
 
+// // owl carousel script
+// $('.carousel').owlCarousel({
+//     margin: 20,
+//     loop: true,
+//     autoplay: true,
+//     autoplayTimeOut: 2000,
+//     autoplayHoverPause: true,
+//     responsive: {
+//         0:{
+//             items: 1,
+//             nav: false
+//         },
+//         600:{
+//             items: 2,
+//             nav: false
+//         },
+//         1000:{
+//             items: 3,
+//             nav: false
+//         }
+//     }
+// });
+
 
 
 
